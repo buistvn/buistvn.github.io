7765009f166ff1c6b8d853d7bc6f96d46abbc468
Steven Bui <bui.stvn@gmail.com>
2022-03-23 11:53:30 -0700

    Set up routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,20 @@ import { HashRouter, Route, Routes } from 'react-router-dom';
 import { ChakraProvider, theme } from '@chakra-ui/react';
 
 import { Navbar } from './components/Navbar';
+import { About } from './pages/About';
+import { Home } from './pages/Home';
+import { Projects } from './pages/Projects';
 
 export const App = () => {
     return (
         <ChakraProvider theme={theme}>
             <HashRouter>
                 <Navbar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/projects" element={<Projects />} />
+                </Routes>
             </HashRouter>
         </ChakraProvider>
     );
