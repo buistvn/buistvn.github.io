122526c39590c23d7b57940afe9f4001bea5c66f
Steven Bui <bui.stvn@gmail.com>
2022-03-30 22:17:45 -0700

    Fix borders on ProjectPlaceholder

--- src/components/ProjectPlaceholder.tsx
+++ src/components/ProjectPlaceholder.tsx
@@ -1,22 +1,24 @@
-import { AspectRatio, Box, useColorModeValue } from '@chakra-ui/react';
+import { Box, useColorModeValue, useMediaQuery } from '@chakra-ui/react';
 
 export const ProjectPlaceholder = () => {
     const borderColor = useColorModeValue('gray.200', 'gray.700');
     const placeholderColor = useColorModeValue('gray.100', 'gray.800');
 
-    return (
-        <AspectRatio
-            ratio={16 / 9}
-            maxWidth={['100%', '100%', '100%', '100%', '80%']}
-        >
+    const [isLargerThan1200] = useMediaQuery('(min-width: 1200px)');
+
+    if (isLargerThan1200) {
+        return (
             <Box
                 border="1px"
                 borderColor={borderColor}
-                borderTopRadius="16px"
-                borderBottomRadius={['0px', '0px', '0px', '0px', '16px']}
+                borderRadius="16px"
                 boxShadow="md"
                 backgroundColor={placeholderColor}
             />
-        </AspectRatio>
-    );
+        );
+    } else {
+        return (
+            <Box borderTopRadius="16px" backgroundColor={placeholderColor} />
+        );
+    }
 };
