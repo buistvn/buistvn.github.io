8cc8dd3a65b361073b39f267c3f576689d14fc8e
Steven Bui <bui.stvn@gmail.com>
2022-08-20 18:34:16 -0700

    Update project descriptions

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -23,7 +23,7 @@ export const projects: Project[] = [
     {
         name: 'Tarpaulin',
         description:
-            'A RESTful API that stores and provides data for a learning management system',
+            'RESTful API that stores and provides data for a learning management system',
         tags: [
             tags['javascript'],
             tags['express'],
@@ -40,7 +40,7 @@ export const projects: Project[] = [
     {
         name: 'Game Collection',
         description:
-            'A mobile app that lets users browse video games and manage their game collections',
+            'Mobile app that lets users browse video games and manage their game collections',
         tags: [tags['kotlin'], tags['androidstudio']],
         image: 'https://raw.githubusercontent.com/buistvn/game-collection/main/docs/game-collection.png',
         repoLink: 'https://github.com/buistvn/game-collection',
@@ -49,7 +49,7 @@ export const projects: Project[] = [
     {
         name: 'MUVI',
         description:
-            'A web app that lets users discover movies and keep track of their favorites and watchlists',
+            'Web app that lets users discover movies and keep track of their favorites and watchlists',
         tags: [
             tags['javascript'],
             tags['react'],
@@ -63,7 +63,7 @@ export const projects: Project[] = [
     {
         name: 'Solar Sim',
         description:
-            'A 3D simulator that displays an interactive model of the solar system',
+            '3D simulator that displays an interactive model of the solar system',
         tags: [tags['cplusplus'], tags['opengl']],
         image: 'https://raw.githubusercontent.com/buistvn/solar-sim/main/docs/solar-sim.png',
         repoLink: 'https://github.com/buistvn/solar-sim',
@@ -71,7 +71,7 @@ export const projects: Project[] = [
     },
     {
         name: 'IMDb Scraper',
-        description: 'A script that extracts and stores movie data from IMDb',
+        description: 'Script that extracts and stores movie data from IMDb',
         tags: [tags['python'], tags['mysql']],
         image: 'https://raw.githubusercontent.com/buistvn/imdb-scraper/main/docs/imdb-scraper.png',
         repoLink: 'https://github.com/buistvn/imdb-scraper',
@@ -80,7 +80,7 @@ export const projects: Project[] = [
     {
         name: 'Lainavi',
         description:
-            'A Discord bot that features commands for moderation, alerts, and gaming',
+            'Discord bot that features commands for moderation, alerts, and gaming',
         tags: [tags['javascript'], tags['nodejs']],
         image: 'https://raw.githubusercontent.com/buistvn/lainavi/main/docs/lainavi.png',
         repoLink: 'https://github.com/buistvn/lainavi',
@@ -89,7 +89,7 @@ export const projects: Project[] = [
     {
         name: 'stevenbui.dev',
         description:
-            "A personal website that showcases the projects I've been working on",
+            "Personal website that showcases the projects I've been working on",
         tags: [tags['typescript'], tags['react'], tags['chakraui']],
         image: 'https://raw.githubusercontent.com/buistvn/stevenbui.dev/main/docs/stevenbui.dev.png',
         repoLink: 'https://github.com/buistvn/stevenbui.dev',
@@ -98,7 +98,7 @@ export const projects: Project[] = [
     {
         name: 'CLOUDVOTE',
         description:
-            'A web app that lets users create proposals for topics that they care about',
+            'Web app that lets users create proposals for topics that they care about',
         tags: [
             tags['javascript'],
             tags['react'],
